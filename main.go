f01ad697eb43db264d1936f3566e473364c40208
文森特 <wstntgg@gmail.com>
2025-03-16 00:49:16 +0100

    Setting up the port in the main file

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("$PORT must be set")
+	}
+	fmt.Println("PORT: ", portString)
 }

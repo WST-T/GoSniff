7074b5ba35270d02498df29cbb9e6f7adc879991
文森特 <wstntgg@gmail.com>
2025-03-17 11:53:44 +0100

    Setting up a timer for the http.server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
@@ -36,8 +37,11 @@ func main() {
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
+		Handler:      router,
+		Addr:         ":" + portString,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
 	log.Printf("Server is running on port %v", portString)
